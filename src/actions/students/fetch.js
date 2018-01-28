e9ba9c7adfe6342d381358fdb84824b8f475da90
brunadafonseca <brunadafonseca@gmail.com>
2018-01-28 16:42:05 +0100

    Cleaning up

---
+++ src/actions/students/fetch.js
@@ -0,0 +1,15 @@
+// import API from '../../api/client'
+// import {
+//   APP_LOADING,
+//   APP_DONE_LOADING,
+//   LOAD_ERROR,
+//   LOAD_SUCCESS
+// } from '../loading'
+//
+// export const BATCH_UPDATED = 'BATCH_UPDATED'
+// export const STUDENT_UPDATED = 'STUDENT_UPDATED'
+// export const STUDENTS_UPDATED = 'STUDENTS_UPDATED'
+// export const UPDATED_BATCH_PERFORMANCE = 'UPDATED_BATCH_PERFORMANCE'
+// export const BATCH_REMOVED = 'BATCH_REMOVED'
+//
+// const api = new API()
---
+++ src/actions/students/update.js
@@ -0,0 +1,14 @@
+// import API from '../../api/client'
+// import {
+//   APP_LOADING,
+//   APP_DONE_LOADING,
+//   LOAD_ERROR,
+//   LOAD_SUCCESS
+// } from '../loading'
+//
+// export const STUDENT_UPDATED = 'STUDENT_UPDATED'
+// export const STUDENTS_UPDATED = 'STUDENTS_UPDATED'
+// export const UPDATED_BATCH_PERFORMANCE = 'UPDATED_BATCH_PERFORMANCE'
+// export const STUDENT_REMOVED = 'STUDENT_REMOVED'
+//
+// const api = new API()
